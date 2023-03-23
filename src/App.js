a92139333fe63ed88cad49c02be95baefdfe255d
saksham-accio <125554544+saksham-accio@users.noreply.github.com>
2023-03-23 06:21:28 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class CounterApp extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Counter App</h1>
+        <p>Count:0</p>
+        <button>Count</button>
+        <button>Reset</button>
+      </div>
+    );
+  }
+}
+
+export default CounterApp;
